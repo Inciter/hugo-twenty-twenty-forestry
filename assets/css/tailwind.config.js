7bf71f77f2db45e65c72620af82eb09616a763f0
Jc Murray <jc.murray@technomanci.com>
2020-10-30 08:19:27 -0600

    feat: Adding new static assets for theming

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    theme: {
+      extend: {
+        inset: {
+          '90': '90px'
+        },
+        colors:{
+          orange: "#dd6c37",
+          gray: {
+            100: "#f1f1f1",
+            800: "#333" 
+          }
+        },
+        fontSize: {
+          '4xl': "2.5rem",
+          '5xl': "2.75rem",
+          '6xl': "3rem"
+        }
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
